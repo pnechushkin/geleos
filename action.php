200bbf7971c0619b19bfa7727812eb9a4e299c07
pnechushkin <pnechushkin@gmail.com>
2017-03-14 16:59:37 +0200

    done

---
+++ action.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Павел
+ * Date: 13.03.2017
+ * Time: 15:49
+ */
---
+++ classes/robots.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Павел
+ * Date: 13.03.2017
+ * Time: 15:10
+ */
+class Robots {
+    private $robots_existence;
+    private $robots_size;
+    private $robots_response;
+    private $host_existence;
+    private $host_quantity;
+    private $sitemap_existence;
+
+
+}
